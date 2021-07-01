4e3f8a950a0d42b9d5d9bc72fe5571d8050b5a22
MattCrw <matheuscrw@yahoo.com>
2021-07-01 18:45:09 -0300

    Final Updates

--- www/js/classes/modelController/controller.js
+++ www/js/classes/modelController/controller.js
@@ -2,18 +2,21 @@ class Controller
 {
     constructor()
     {
+        //set the event to set the score and to add points
         emitter.on(constants.SET_SCORE, this.setScore);
         emitter.on(constants.ADD_POINTS, this.upPoints);
     }
-
+ 
+    //function to set the score
     setScore(score)
     {
         model.score = score;
     }
 
+    //function to add points
     upPoints(points)
     {
-        var score = model.score;
+        var score = model.score; //10
 
         score += points;
         model.score = score;
